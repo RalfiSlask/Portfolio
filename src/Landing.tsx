49dbcd3821bc8f434508b67e740250c93597d6df
Matthias Nilsson <112242026+RalfiSlask@users.noreply.github.com>
2023-10-01 12:38:56 +0200

    1st oct

---
+++ src/Landing.tsx
@@ -0,0 +1,48 @@
+// components/Landing.tsx
+import React from 'react';
+
+const Landing: React.FC = () => {
+  return (
+    <div className="container">
+      <header className="header">
+        <div className="logo">M</div>
+        <nav className="navigation">
+          <a href="#about">About</a>
+          <a href="#work">Work</a>
+          <a href="#projects">Side Projects</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </header>
+      
+      <main className="main-content">
+        <section id="landing">
+          <h1>Matthias Nilsson</h1>
+          <p>Software developer inspired by the digital age.</p>
+        </section>
+        
+        <section id="about">
+          {/* Your About content here */}
+        </section>
+        
+        <section id="work">
+          {/* Your Work or Projects content here */}
+        </section>
+        
+        <section id="projects">
+          {/* Your Side Projects content here */}
+        </section>
+        
+        <section id="contact">
+          {/* Your Contact content here */}
+        </section>
+        
+        <footer>
+            
+          {}
+        </footer>
+      </main>
+    </div>
+  );
+}
+
+export default Landing;
